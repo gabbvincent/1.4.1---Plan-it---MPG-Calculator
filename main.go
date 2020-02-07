8cb702eaf137200c5eefca48c5ec8b1cb8c17750
Vincent Gabb <vincentgabb@gmail.com>
2020-02-07 22:08:00 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+//Programmer: Vincent Gabb
+//1.4.1 - Plan it
+//MPG calculator
+
+//Declare a variable for Gas Tank Size.
+
+//Declare a variable for Miles Travled.
+
+//Ask the user to enter their gas tank size.
+
+//Ask the user to enter the amount of miles they travled.
+
+//output the average gas mileage 
+
+//If the average is less that 20mpg display a message a dissapointed message.
+
+//If the average is more than 20mpg display a message that expresses happiness.
+
+//end.
